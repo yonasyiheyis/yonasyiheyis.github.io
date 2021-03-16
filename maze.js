a0699036ac6d79124e131691f5bb16db1cf64160
yonasyiheyis <49254355+yonasyiheyis@users.noreply.github.com>
2021-03-15 21:27:52 -0500

    maze added

---
+++ maze.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    var start = false;
+
+    $("#start").click(function (){
+        start = true;
+        $("#status").text("Game Has Started!");
+		$(".boundary").removeClass("lost");
+    });
+
+    $(".boundary").mouseover(function() {
+		lostGame();
+	});
+
+    $("body").mouseover(function() {
+		if (event.target.id != ("maze") && event.target.id != ("start") && event.target.id != ("end")) {
+			lose();
+		}
+	});
+
+    $("#end").mouseover(function() {
+		if (start) {
+			start = false;
+			$("#status").text("You win!");
+		}
+	});
+
+    function lostGame() {
+		if (start) {
+			start = false;
+			$("#status").text("You lose!");
+			$(".boundary").addClass("lost");
+		}
+	}
+
+});
